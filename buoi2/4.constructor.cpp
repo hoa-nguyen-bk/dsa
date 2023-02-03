0618174950a68c2f17baff9d205a7a21d2a3f427
hoaminzy <48762609+student1060632fpt@users.noreply.github.com>
2023-02-03 18:34:28 +0700

    constructor - destructor

--- buoi2/4.constructor.cpp
+++ buoi2/4.constructor.cpp
@@ -5,25 +5,33 @@ using namespace std;
 // - class
 // - các thuộc tính oop
 // +Tính đóng gói encapsulation: gom tất cả và gói gọn lại trong class student
+// +Constructor
 class student
 {
- private:
-
+private:
   int ID = 10;
-public:          // các đối tượng bên ngoài có thể truy cập vào bên trong class
+
+public: // các đối tượng bên ngoài có thể truy cập vào bên trong class
+  // constructor
+  student(int ID)
+  {
+    this->ID = ID;
+    cout << "Hello" << endl;
+  }
+  // destructor
+  ~student()
+  {
+    cout << "Good bye" << endl;
+  }
   void callName()
   {
     cout << ID;
   }
-
-  
 };
 int main()
 {
-  student A;
+  student A(5);
   A.callName(); // có hiện duy nhất name vì nó được quyền truy cập vào name
-  //vậy nếu muốn id ko đổi được thì để id vào private
-  A.ID = 20; //nên chỗ này báo lõi là đúng
-  A.callName();
+  // vậy nếu muốn id ko đổi được thì để id vào private
   return 0;
 }
