2bebd332f69cbfc0828004db0fc84355e7bb2cb3
hoaminzy <48762609+student1060632fpt@users.noreply.github.com>
2023-02-03 18:45:51 +0700

    template hàm bị repeat

---
+++ buoi2/5.template.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Nội dung buổi 2
+// - class
+// - các thuộc tính oop
+// +Tính đóng gói encapsulation: gom tất cả và gói gọn lại trong class student
+// +Constructor
+// +Inheritance -> kế thừa
+// DRY => dont repeat yourself
+// + template
+
+int sum(int a, int b){
+
+};
+
+float sum(float a, float b){
+
+};
+
+double sum(double a, double b){
+
+};
+
+class SSL
+{
+};
+
+int main()
+{
+
+  return 0;
+}
