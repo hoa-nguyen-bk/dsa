628867a7e7baf0244d207a1ff3c5df58247f03d4
hoaminzy <48762609+student1060632fpt@users.noreply.github.com>
2023-02-09 23:31:02 +0700

    thử typedef với struct

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dsa
+
+1. nhấn `ctrl+shift+b` cho việc compile ra .exe
+2. nhấn `ctrl+alt+n` cho việc compile ra và cả run luôn, nhanh gấp đôi
+
--- test1_basic_programming_code/q3_typedef_ex.cpp
+++ test1_basic_programming_code/q3_typedef_ex.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 using namespace std;
 
-//typedef giúp cho code dễ đọc dễ maintain, alias ptr có ý nghĩa hơn int * trong cái ex này
-typedef int *ptr;
+// typedef giúp cho code dễ đọc dễ maintain, alias ptr có ý nghĩa hơn int * trong cái ex này
+// typedef int *ptr;
+typedef struct Item* ptr;
+// bi giờ cho thêm cái struct mới mệt
+struct Item
+{
+  int value;
+  //trong chatgpt kêu cái dòng dưới thay thế cho int *next tại có cái typedef bên trên rồi
+  ptr next;
+};
 
 int main()
 {
-  ptr x;
-  int y = 10;
+  // ptr x;
+  // int y = 10;
   // gán địa chỉ y cho x
-  x = &y;
+  // x = &y;
   // giá trị y được truy cập thông qua con trỏ x
-  cout << *x << endl;
+  // cout << *x << endl;
+
+  ptr head;
+  head = new Item;
+  //This code creates a pointer head  and assigns it a new instance of the Item  class. head  is now a pointer to the newly created Item instance.
+  // head là một con trỏ tới đối tượng Item mới được tạo
+  head->value = 5;
+  head->next = nullptr;
+  //nullptr is a keyword in C++ that represents a null pointer. It is used to represent a pointer that does not point to an object or a function. It is often used to indicate an empty or invalid pointer, and can be used to initialize a pointer to an invalid value.
+  cout << head->next << endl;
+
+
   return 0;
 }
