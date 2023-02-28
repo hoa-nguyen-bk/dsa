d48100396e92ca2a6ac4661b563215fe5d07f975
hoa nguyen <hoalenguyen1999@gmail.com>
2023-03-01 01:54:09 +0700

    xoas code cux

--- lab/week1/recursion/bai6.cpp
+++ lab/week1/recursion/bai6.cpp
@@ -4,84 +4,81 @@
 
 using namespace std;
 
-string expand(string s) {
-  int length = s.length();
-    if (length == 0) {
-        return "";
-    } else if (length == 1) {
-        return s;
-    } else if (isdigit(s[0])) {
-        int num = stoi(s[0]);//chuyển thành số cho cái số đầu tiên
-        if(num == 1){
-          //nếu số đầu là số 1 thì cắt số 1 và dấu ()
-          return s.substr(2, length-2);
-        }
-        string str = "";
-        int i = 2; // Skip the opening bracket
-        while (s[i] != ')') {
-            str += s[i];
-            i++;
-        }
-        string rest = s.substr(i + 1);
-        return string(num, expand(str)) + expand(rest);
-    } else {
-        return s[0] + expand(s.substr(1));
-    }
-}
-
-
-// chat gpt ver
-string expandwhile(string s)
+// cần thêm hàm repeat đệ quy phụ cho
+string repeat(string s, int n)
 {
-  int i = 0;
-  string res = "";
-
-  while (i < s.length())
+  if (n == 1)
   {
-    if (isdigit(s[i]))
-    {
-      int n = s[i] - '0';
-      i += 2; // skip the digit and the '('
-      int j = i, cnt = 1;
-      while (cnt > 0)
-      {
-        if (s[j] == '(')
-          cnt++;
-        else if (s[j] == ')')
-          cnt--;
-        j++;
-      }
-      string p = s.substr(i, j - i - 1);
-      for (int k = 0; k < n; k++)
-      {
-        res += expand(p);
-      }
-      i = j;
-    }
-    else if (islower(s[i]))
-    {
-      res += s[i];
-      i++;
-    }
-    else
-    {
-      i++;
-    }
+    return s;
   }
-
-  return res;
+  else
+  {
+    return s + repeat(s, n - 1);
+  }
 }
 
-// my ver
-string myexpand(string s)
+// string expand1(string s) {
+//   int length = s.length();
+//     if (length == 0) {
+//         return "";
+//     } else if (length == 1) {
+//         return s;
+//     } else if (isdigit(s[0])) {
+//         int num = stoi(s[0]);//chuyển thành số cho cái số đầu tiên
+//         if(num == 1){
+//           //nếu số đầu là số 1 thì cắt số 1 và dấu ()
+//           return s.substr(2, length-2);
+//         }
+//         string str = "";
+//         int iLast = length-2; // Skip the opening bracket
+
+//         return "string(num, expand(str)) + expand(rest)";
+//     } else {
+//         return s[0] + expand(s.substr(1));
+//     }
+// }
+string expand(string s)
 {
+  if (s.length() == 0)
+  {
+    return "";
+  }
+  else if (s.length() == 1)
+  {
+    return s;
+  }
+  else if (isdigit(s[0]))
+  {
+    int num = s[0] - '0';
+    string str = "";
+    int i = 2; // Skip the opening bracket
+    int count = 1;
+    while (count != 0)
+    {
+      if (s[i] == '(')
+      {
+        count++;
+      }
+      else if (s[i] == ')')
+      {
+        count--;
+      }
+      str += s[i];
+      i++;
+    }
+    string rest = s.substr(i);
+    return repeat(expand(str.substr(1, str.length() - 2)), num) + expand(rest);
+  }
+  else
+  {
+    return s[0] + expand(s.substr(1));
+  }
 }
 
 int main()
 {
-  int a, b;
-  cout << string(3, "he") <<endl;
-  // cout << expand("2(ab3(cde)x)") << endl; // output: abcdecdecdexabcdecdecdex
-  // cout << expand("2(x0(y))3(z)") << endl; // output: xxzzz
+
+  cout << expand("2(ab3(cde)x)") << endl; // output: abcdecdecdexabcdecdecdex
+  cout << expand("2(x0(y))3(z)") << endl; // output: xxzzz
   return 0;
 }
