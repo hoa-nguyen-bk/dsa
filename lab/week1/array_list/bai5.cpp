a3e0b9ef603673149f2de2c30d1cfef12c7dd1ae
hoa nguyen <hoalenguyen1999@gmail.com>
2023-03-02 18:04:34 +0700

    xong

--- lab/week1/array_list/bai5.cpp
+++ lab/week1/array_list/bai5.cpp
@@ -6,8 +6,11 @@ using namespace std;
 int equalSumIndex(vector<int> &nums)
 {
   int numsLength = (int)nums.size();
-  cout << "numsLength = " << numsLength << endl;
-  if (numsLength <= 2)
+  if (numsLength == 0)
+    return -1;
+  if (numsLength == 1)
+    return 0;
+  if (numsLength == 2)
     return -1;
   int index = -1;
   for (int i = 1; i < numsLength; i++)
@@ -16,18 +19,14 @@ int equalSumIndex(vector<int> &nums)
     int sumRight = 0;
     for (int left = 0; left < i; left++)
     {
-      cout << "nums[" << left << "] = " << nums[left] << endl;
       sumLeft += nums[left];
     }
     for (int right = numsLength - 1; right > i; right--)
     {
-      cout << "nums[" << right << "] = " << nums[right] << endl;
       sumRight += nums[right];
     }
-    cout << "sumLeft = " << sumLeft << "; sumRight = " << sumRight << endl;
     if (sumLeft == sumRight)
     {
-      cout << "index = " << index << endl;
       index = i;
     }
   }
