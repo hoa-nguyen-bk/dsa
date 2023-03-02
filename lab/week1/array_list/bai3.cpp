6b60221443e26ced3acbf21378c0f5147708bb38
hoa nguyen <hoalenguyen1999@gmail.com>
2023-03-02 16:19:09 +0700

    sửa lại xí bài 3

--- lab/week1/array_list/bai3.cpp
+++ lab/week1/array_list/bai3.cpp
@@ -4,17 +4,23 @@ using namespace std;
 
 int buyCar(int *nums, int length, int k)
 {
+  if (length == 0)
+    return 0;
   sort(nums, nums + length);
+  int i = 0, things = 0, sum = 0;
   cout << "sortArr = " << endl;
   for (int i = 0; i < length; i++)
   {
     cout << "arr[" << i << "]" << nums[i] << endl;
   }
-  int i = 0, things = 0;
-  while (k > 0)
+  while (i < length)
   {
-    things++;
-    k = k - nums[i];
+    if (sum + nums[i] < k)
+    {
+      things++;
+      sum = sum + nums[i];
+      cout << "k = " << k << endl;
+    }
     i++;
   }
   return things;
@@ -22,7 +28,8 @@ int buyCar(int *nums, int length, int k)
 
 int main()
 {
-  int nums[] = {90, 30, 40, 90, 20};
+  // int nums[] = {90, 30, 40, 90, 20};
+  int nums[] = {10, 30, 40, 90, 20};
   int length = sizeof(nums) / sizeof(nums[0]);
   cout << buyCar(nums, length, 90) << "\n";
   // 3
