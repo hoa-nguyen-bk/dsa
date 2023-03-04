b6817aeb8a632ef017cd5883637b357f1a627f1f
hoa nguyen <hoalenguyen1999@gmail.com>
2023-03-04 15:03:31 +0700

    sắp xong câu 1

--- lab/week1/singly_linked_list/bai1.cpp
+++ lab/week1/singly_linked_list/bai1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 template <class T>
 class SLinkedList
 {
@@ -12,9 +13,23 @@ protected:
 public:
   SLinkedList();
   ~SLinkedList();
-  void add(T e);
-  void add(int index, T e);
+  void add(const T &e);
+  void add(int index, const T &e);
   int size();
+  int *toString()
+  {
+    Node *tmp = head;
+    // nếu mà viết tmp->next thế này thì nó còn lâu mới đụng zô được thằng cuối cùng. Nên là phải dùng tmp != null pointer thôi.
+    cout << "[";
+    while (tmp != nullptr)
+    {
+      cout << tmp->data << ", ";
+      tmp = tmp->next;
+    }
+    cout << "]" << endl;
+
+    return 0;
+  }
 
 public:
   class Node
@@ -40,27 +55,82 @@ public:
     }
   };
 };
+
+template <class T>
+SLinkedList<T>::SLinkedList()
+{
+  head = nullptr;
+  tail = nullptr;
+  count = 0;
+}
+
+template <class T>
+SLinkedList<T>::~SLinkedList()
+{
+  Node *current = head;
+  while (current != nullptr)
+  {
+    Node *next = current->next;
+    delete current;
+    current = next;
+  }
+  head = nullptr;
+  tail = nullptr;
+  count = 0;
+}
+
 template <class T>
 void SLinkedList<T>::add(const T &e)
 {
-  /* Insert an element into the end of the list. */
-  
+  /* Insert an element into the end of the list */
+  Node *newElement = new Node(e, nullptr);
+  if (this->head == nullptr)
+  {
+    this->head = newElement;
+    this->tail = newElement;
+  }
+  else
+  {
+    this->tail->next = newElement;
+    this->tail = newElement;
+  }
+  this->count++;
 }
 
 template <class T>
 void SLinkedList<T>::add(int index, const T &e)
 {
-  /* Insert an element into the list at given index. */
+  /* Insert an element into the list at given index */
+  if (index < 0 || index > this->count)
+  {
+    cout << "Index out of range" << endl;
+    return;
+  }
+  Node *newElement = new Node(e, nullptr);
+  if (index == 0)
+  {
+    newElement->next = this->head;
+    this->head = newElement;
+  }
+  else
+  {
+    Node *current = this->head;
+    for (int i = 0; i < index - 1; i++)
+    {
+      current = current->next;
+    }
+    newElement->next = current->next;
+    current->next = newElement;
+  }
+  this->count++;
 }
 
 template <class T>
 int SLinkedList<T>::size()
 {
   /* Return the length (size) of list */
-  return 0;
+  return this->count;
 }
-
-using namespace std;
 
 int main()
 {
@@ -69,10 +139,10 @@ int main()
 
   for (int index = 0; index < size; index++)
   {
-    list.add(index);
+    list.add(0, index);
   }
 
   cout << list.toString();
-  // [0,1,2,3,4,5,6,7,8,9]
+  // [9,8,7,6,5,4,3,2,1,0]
   return 0;
 }
