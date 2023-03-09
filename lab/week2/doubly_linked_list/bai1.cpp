43b940c47c5042d16f3fcca1e34be79641efca7b
hoa nguyen <hoalenguyen1999@gmail.com>
2023-03-10 02:00:40 +0700

    trước khi commit

--- lab/week2/doubly_linked_list/bai1.cpp
+++ lab/week2/doubly_linked_list/bai1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 template <class T>
 class DLinkedList
@@ -18,6 +18,7 @@ public:
   void add(int index, const T &e);
   int size();
   int *toString();
+  void toNodeString();
 
 public:
   class Node
@@ -44,7 +45,18 @@ public:
   };
 };
 
+template <class T>
+DLinkedList<T>::DLinkedList()
+{
+  this->head = nullptr;
+  this->tail = nullptr;
+  this->count = 0;
+}
 
+template <class T>
+DLinkedList<T>::~DLinkedList()
+{
+}
 
 template <class T>
 int *DLinkedList<T>::toString()
@@ -62,22 +74,118 @@ int *DLinkedList<T>::toString()
 }
 
 template <class T>
+void DLinkedList<T>::toNodeString()
+{
+  // Print the table header
+  std::cout << "+-------+-------+-------+-------+\n";
+  std::cout << "| Stt   | Prev  | Value | Next  |\n";
+  std::cout << "+-------+-------+-------+-------+\n";
+
+  // Print each node in the linked list
+  int i = 0;
+  Node *current_node = this->head;
+  while (current_node != nullptr)
+  {
+    std::cout << "| " << std::setw(5) << i
+              << " | ";
+    if (current_node->previous == nullptr)
+    {
+      std::cout << std::setw(5) << "start"
+                << " | ";
+    }
+    else
+    {
+      std::cout << std::setw(5) << current_node->previous->data << " | ";
+    }
+    std::cout << std::setw(5) << current_node->data << " | ";
+
+    if (current_node->next == nullptr)
+    {
+      std::cout << std::setw(5) << "end"
+                << " |\n";
+    }
+    else
+    {
+      std::cout << std::setw(5) << current_node->next->data << " |\n";
+    }
+    current_node = current_node->next;
+    i++;
+  }
+
+  // Print the table footer
+  std::cout << "+-------+-------+-------+-------+\n";
+}
+
+template <class T>
 void DLinkedList<T>::add(const T &e)
 {
   /* Insert an element into the end of the list. */
+  Node *newNode = new Node(e);
+  if (this->count != 0)
+  {
+    this->tail->next = newNode;
+    newNode->previous = this->tail;
+  }
+  else
+  {
+    this->head = newNode;
+  }
+  // cout<<"this->tail = "<<this->tail<<"; this->he = " << this->head << " ->" <<this->count<<endl;
+  this->tail = newNode;
+  this->count++;
 }
 
 template <class T>
 void DLinkedList<T>::add(int index, const T &e)
 {
   /* Insert an element into the list at given index. */
+  Node *newNode = new Node(e);
+  // lần đầu khi mảng chưa có gì thì zô đây
+  if (this->count == 0)
+  {
+    this->head = newNode;
+    this->tail = newNode;
+  }
+  // lần 2 sẽ là zô đít
+  else if (this->count == index)
+  {
+    this->tail->next = newNode;
+    newNode->previous = this->tail;
+  }
+  // lần 3 zô khi mảng đã có hàng họ đủ rồi
+  else
+  {
+    int i = 0;
+
+    Node *tmp = this->head;
+    while (tmp != nullptr)
+    {
+      // cout << "; tmp->next->next = " << tmp->next << endl;
+
+      if (i == index)
+      {
+        newNode->next = tmp->next;
+        if (tmp->next != nullptr)
+        {
+          tmp->next->previous = newNode;
+        }
+        tmp->next = newNode;
+        newNode->previous = tmp;
+        cout << "newNode->next = " << newNode->next << "; tmp->next->next = " << tmp->next->next << endl;
+        break;
+      }
+      i++;
+      tmp = tmp->next;
+    }
+  }
+  this->count++;
 }
 
 template <class T>
 int DLinkedList<T>::size()
 {
   /* Return the length (size) of list */
-  return 0;
+  return this->count;
 }
 
 int main()
@@ -86,9 +194,11 @@ int main()
   int size = 10;
   for (int idx = 0; idx < size; idx++)
   {
-    list.add(idx);
+    // list.add(idx);
+    list.add(0, idx);
   }
-  cout << list.toString();
+  cout << list.toString() << endl;
+  list.toNodeString();
   // [0,1,2,3,4,5,6,7,8,9]
   return 0;
 }
---
+++ lab/week2/doubly_linked_list/tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+  // int i = 0;
+
+    // Node *tmp = this->head;
+    // while (tmp != nullptr)
+    // {
+    //   cout << "newNode->next = " << newNode->next << "; tmp->next->next = " << tmp->next->next << endl;
+    //   if (i == index)
+    //   {
+    //     // newNode->next = tmp->next->next;
+    //     // newNode->previous = tmp;
+    //     // tmp->next->previous = newNode;
+    //     // tmp->next = newNode;
+    //     break;
+    //   }
+    //   i++;
+    //   tmp = tmp->next;
+    // }
