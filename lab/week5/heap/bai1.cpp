78d2a19d9295b44acbf80e32b29fa3e3487a2828
student1060632fpt <haveaniceday6174@gmail.com>
2023-05-02 08:09:57 +0700

    lưu heap nhưng chưa đc full điểm

--- lab/week5/heap/bai1.cpp
+++ lab/week5/heap/bai1.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #include <algorithm>
 using namespace std;
 #define SEPARATOR "#<ab@17943918#@>#"
+// https://www.programiz.com/dsa/heap-data-structure - tham khảo hay
 template <class T>
 class Heap
 {
@@ -21,9 +22,9 @@ protected:
 public:
   Heap()
   {
-    this->capacity = 10;
-    this->count = 0;
-    this->elements = new T[capacity];
+    capacity = 10;
+    count = 0;
+    elements = new T[capacity];
   }
   ~Heap()
   {
@@ -36,7 +37,14 @@ public:
   T peek();
   bool pop();
   int size();
-
+  int leftchild(int pos) const
+  {
+    return 2 * pos + 1;
+  } // Return leftchild position
+  int rightchild(int pos) const
+  {
+    return 2 * pos + 2;
+  } // Return rightchild position
   void printHeap()
   {
     cout << "Max Heap [ ";
@@ -44,13 +52,42 @@ public:
       cout << elements[i] << " ";
     cout << "]\n";
   }
+  bool isLeaf(int pos) const // True if pos is a leaf
+  {
+    return (pos >= count / 2) && (pos < count);
+  }
 
 private:
   void ensureCapacity(int minCapacity);
   void reheapUp(int position);
-  void reheapUp2(int position);
   void reheapDown(int position);
 };
+template <typename E, typename T>
+inline void swap(E A[], T i, T j)
+{
+  E temp = A[i];
+  A[i] = A[j];
+  A[j] = temp;
+}
+
+template <class T>
+void Heap<T>::reheapDown(int position)
+{
+  while (!isLeaf(position))
+  {
+    // stop if position is leaf
+    int j = leftchild(position);
+    int rc = rightchild(position);
+    if ((rc < count) && (elements[rc] < elements[j]))
+    {
+      j = rc; // set j to greater child's value
+    }
+    if (elements[position] < elements[j])
+      return;
+    swap(elements, position, j);
+    position = j; // move down
+  }
+}
 // Your code goes here
 // Bài 2
 int parent(int pos) // return parent position
@@ -58,13 +95,6 @@ int parent(int pos) // return parent position
   return (pos - 1) / 2;
 }
 // Swap two elements in a generic array
-template <typename E>
-inline void swap(E A[], int i, int j)
-{
-  E temp = A[i];
-  A[i] = A[j];
-  A[j] = temp;
-}
 
 template <class T>
 void Heap<T>::push(T item)
@@ -76,23 +106,13 @@ void Heap<T>::push(T item)
   }
   int current = count++; // current = count trước, count ++ sau
 
-  cout << "count = " << count << endl;
-  cout << "current = " << current << endl;
-
   elements[current] = item; // start at end of heap
   // Giờ sift up cho đến khi current parent > current
   reheapUp(current);
-
-  // while ((current != 0) && (elements[current] > elements[parent(current)]))
-  // {
-  //   swap(elements, current, parent(current));
-  //   current = parent(current);
-  // }
 }
 template <class T>
 void Heap<T>::ensureCapacity(int minCapacity)
 {
-  cout << "ensure" << endl;
   if (minCapacity > capacity)
   {
     int newCapacity = (capacity * 3) / 2 + 1;
@@ -110,8 +130,6 @@ void Heap<T>::reheapUp(int position)
   int parentPosition = parent(position);
   if (position != 0 && elements[position] > elements[parentPosition])
   {
-    cout << "position[" << position << "] = " << elements[position] << endl;
-    cout << "parent[" << parentPosition << "] = " << elements[parentPosition] << endl;
     swap(elements, position, parentPosition);
     reheapUp(parentPosition);
   }
@@ -121,27 +139,54 @@ void Heap<T>::reheapUp(int position)
 template <class T>
 int Heap<T>::size()
 {
+  return count;
 }
 
 template <class T>
 bool Heap<T>::isEmpty()
 {
+  if (count == 0)
+    return true;
+  return false;
 }
 
+// Peek (Find max/min)
+// Peek operation returns the maximum element from Max Heap or minimum element from Min Heap without deleting the node.
+// For both Max heap and Min Heap
 template <class T>
 T Heap<T>::peek()
 {
+  if (isEmpty())
+    return T();
+  return elements[0];
 }
 
 template <class T>
 bool Heap<T>::contains(T item)
 {
+  if (isEmpty())
+    return false;
+  for (int i = 0; i < size(); i++)
+  {
+    if (elements[i] == item)
+    {
+      return true;
+    }
+  }
+  return false;
 }
 
 template <class T>
 bool Heap<T>::pop()
 {
+  if (isEmpty())
+    return false;
+  swap(elements, 0, --count); // swap fist with last value
+  if (count != 0)
+    reheapDown(0); // reheap down new root value
+  return true;
 }
+
 int main()
 {
   Heap<int> maxHeap;
@@ -149,10 +194,16 @@ int main()
   {
     maxHeap.push(i);
   }
+  cout << maxHeap.peek() << endl;//4
   maxHeap.printHeap();
   // Max Heap [ 4 3 1 0 2 ]
-  cout << maxHeap.size();    // 10
-  cout << maxHeap.isEmpty(); // 0
-  cout << "Kết thúc êm ái" << endl;
+  cout << maxHeap.size() << endl;       // 10
+  cout << maxHeap.isEmpty() << endl;    // 0
+  cout << maxHeap.contains(4) << endl;  // 1
+  cout << maxHeap.contains(11) << endl; // 0
+  cout << maxHeap.pop() << endl;
+  maxHeap.printHeap();
+  // [3 1 0 2]
+  cout << "ket thuc em ai" << endl;
   return 0;
 }
